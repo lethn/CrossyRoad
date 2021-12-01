18b215a809b9ae1ed9aea1044dd38b0ff83dc0d7
Truong Nhat Anh <tnanh20@apcs.fitus.edu.vn>
2021-12-01 17:10:06 +0700

    Level

--- CrossyRoad/CrossyRoad/Level.h
+++ CrossyRoad/CrossyRoad/Level.h
@@ -2,7 +2,30 @@
 #define _LEVEL_H_
 
 #include "Library.h"
+#include <algorithm>
+#include "ENEMY.h"
+#include "DOG.h"
+#include "TRUCK.h"
+#include "CAR.h"
+//#include "BAT.h"   
 
-
+class LEVEL
+{
+private:
+	int level;
+	int maxEnemy, minSpeed, maxSpeed, curEnemy;
+	const int maxLevel = 5;
+public:
+	LEVEL();
+	LEVEL(int level, int nEnemy);
+	~LEVEL(){};
+	void initialize();
+	bool newLevel(int lv);
+	bool nextLevel();
+	cEnemy * randNewEnemy(cPosition pos);
+	int randSpeed();
+	void decNEnemy(int d);
+	friend class LANE;
+};
 
 #endif
