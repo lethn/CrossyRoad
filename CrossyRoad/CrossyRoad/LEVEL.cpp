ea9c61832d8a39177ed83f429a2283e3ec9024bf
Truong Nhat Anh <tnanh20@apcs.fitus.edu.vn>
2021-12-02 01:30:06 +0700

    update level

--- CrossyRoad/CrossyRoad/LEVEL.cpp
+++ CrossyRoad/CrossyRoad/LEVEL.cpp
@@ -1,74 +1,75 @@
 #include "LEVEL.h"
 
-LEVEL::LEVEL() {
-	level = 1;
-	curEnemy = 0;
-	initialize();
+LEVEL::LEVEL() :
+    level(1),
+    maxEnemy(5), currEnemy(0),
+    minSpeed(700), maxSpeed(900),
+    redLightRate(5), greenLightRate(10) {}
+
+LEVEL::LEVEL(int level, int currEnemy) : level(level), currEnemy(currEnemy) {
+    generateLevel();
 }
 
-LEVEL::LEVEL(int level, int curEnemy) {
-	this->level = level;
-	this->curEnemy = curEnemy;
-	initialize();
-}
-
-void LEVEL::initialize() {
-	switch (level)
-		{
-		case 1: {
-			maxEnemy = 5; // t de theo tam linh
-			maxSpeed = 900;  
-			minSpeed = 700; 
-			break; 
-		}
-		case 2: {
-			maxEnemy = 7; 
-			maxSpeed = 650;  
-			minSpeed = 500; 
-			break;
-		}
-		case 3: {
-			maxEnemy = 9; 
-			maxSpeed = 450;  
-			minSpeed = 300; 
-			break;
-		}
-		case 4: {
-			maxEnemy = 11; 
-			maxSpeed = 350;  
-			minSpeed = 200; 
-			break;
-		}
-		default:
-			maxEnemy = 13; 
-			maxSpeed = 200;  
-			minSpeed = 100; 
-			break;
-		}
+void LEVEL::generateLevel() {
+    switch (level)
+    {
+        case 1:
+            maxEnemy = 5; // t de theo tam linh
+            maxSpeed = 900;
+            minSpeed = 700;
+            redLightRate = 5;
+            greenLightRate = 10;
+            break;
+        
+        case 2:
+            maxEnemy = 7;
+            maxSpeed = 650;
+            minSpeed = 500;
+            redLightRate = 10;
+            greenLightRate = 15;
+            break;
+        
+        case 3:
+            maxEnemy = 9;
+            maxSpeed = 450;
+            minSpeed = 300;
+            redLightRate = 15;
+            greenLightRate = 15;
+            break;
+        
+        case 4:
+            maxEnemy = 11;
+            maxSpeed = 350;
+            minSpeed = 200;
+            redLightRate = 20;
+            greenLightRate = 10;
+            break;
+        
+        default:
+            maxEnemy = 11;
+            maxSpeed = 200;
+            minSpeed = 100;
+            redLightRate = 25;
+            greenLightRate = 10;
+            break;
+    }
 }
 
 bool LEVEL::newLevel(int lv) {
-	if (lv > maxLevel) return false;
-	level = lv;
-	curEnemy = 0;
-	initialize();
-	return true;
+    if (lv > maxLevel) return false;
+    level = lv;
+    currEnemy = 0;
+    generateLevel();
+    return true;
 }
 
 bool LEVEL::nextLevel() {
-	if (level == maxLevel) return false;
-	++level;
-	curEnemy = 0;
-	return true;
+    if (level == maxLevel) return false;
+    ++level;
+    currEnemy = 0;
+    return true;
 }
 
-
-int LEVEL::randSpeed()
-{
-	int k = maxSpeed - minSpeed;
-	return rand() % minSpeed + k ;
-    
-}
 ENEMY *LEVEL::randNewEnemy(int x, int y, bool type) {
     if (currEnemy == maxEnemy)
         return NULL;
@@ -113,10 +114,8 @@ int LEVEL::randSpeed()
     int k = maxSpeed - minSpeed;
     return rand() % minSpeed + k ;
 
-
 }
 
 void LEVEL::decNEnemy(int d) {
     currEnemy -= d;
 }
-
