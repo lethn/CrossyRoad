d82e9576dcdd3dd30a0c864279b7e29a81b5f8ba
Truong Nhat Anh <tnanh20@apcs.fitus.edu.vn>
2021-12-02 12:07:57 +0700

    update

--- CrossyRoad/CrossyRoad/Level.h
+++ CrossyRoad/CrossyRoad/Level.h
@@ -11,25 +11,27 @@
 
 class LEVEL
 {
-private:
-    int level;
-    int    maxEnemy, currEnemy;
-    int minSpeed, maxSpeed;
-    int redLightRate, greenLightRate;
-    const int maxLevel = 5;
-    
-    void generateLevel();
+    private:
+        int level;
+        int    maxEnemy, currEnemy;
+        int minSpeed, maxSpeed;
+        int redLightRate, greenLightRate;
+        const int maxLevel = 5;
 
-public:
-    LEVEL();
-    LEVEL(int level, int currEnemy);
-    ~LEVEL() {};
-    bool newLevel(int lv);
-    bool nextLevel();
-    ENEMY *randNewEnemy(int x, int y, bool type);
-    int randSpeed();
-    void decNEnemy(int d);
+        void generateLevel();
+
+    public:
+        LEVEL();
+        LEVEL(int level, int currEnemy);
+        ~LEVEL() {};
+        bool newLevel(int lv);
+        bool nextLevel();
+        ENEMY *randNewEnemy(int x, int y, short direction);
+        int randSpeed();
+        void decNEnemy(int d);
+
     friend class LANE;
+    friend class MAP;
 };
 
 #endif
